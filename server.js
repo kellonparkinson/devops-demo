d77c9b25fa973c440a654491b3fe33199a9830e1
Kellon Parkinson <kellon.digital@gmail.com>
2022-11-03 12:57:53 -0600

    initial upload

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+require('dotenv').config()
+
+app.use(express.json())
+
+// Rollbar code ----------------------------------
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+// -----------------------------------------------
+
+const students = ['Jimmy', 'Timothy', 'Jimothy']
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/api/students', (req, res) => {
+    res.status(200).send(students)
+})
+
+app.post('/api/students', (req, res) => {
+   let {name} = req.body
+
+   rollbar.info(`Student named ${name} was added.`)
+
+   const index = students.findIndex(student => {
+       return student === name
+   })
+
+   try {
+       if (index === -1 && name !== '') {
+           students.push(name)
+           res.status(200).send(students)
+       } else if (name === ''){
+            rollbar.error('Name field was blank')
+           res.status(400).send('You must enter a name.')
+       } else {
+           res.status(400).send('That student already exists.')
+       }
+   } catch (err) {
+       console.log(err)
+   }
+})
+
+app.delete('/api/students/:index', (req, res) => {
+    const targetIndex = +req.params.index
+    
+    students.splice(targetIndex, 1)
+    res.status(200).send(students)
+})
+
+const { PORT } = process.env
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
